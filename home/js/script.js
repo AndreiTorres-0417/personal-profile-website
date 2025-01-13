85e0c740f3a4f67c9c8d2f499512d8770e7c34ba
AndreiTorres-0417 <thesecondhero0417@gmail.com>
2025-01-13 21:32:00 +0800

    Update script.js

--- home/js/script.js
+++ home/js/script.js
@@ -95,13 +95,14 @@ function changeContent(section) {
         'contact': `
             <h2>Contact</h2>
             <p>
-        I am available to contact mostly on weekdays. You can reach me via my email at 
-        <strong>amtorres2@student.apc.edu.ph</strong> or on Facebook at 
-        <strong><a href="https://www.facebook.com/andreiluis.torres" target="_blank">https://www.facebook.com/andreiluis.torres</a></strong>.
-        Whether you have a question, want to discuss an idea, or just want to get in touch, feel free to send me a message anytime. 
-        I’ll do my best to respond as quickly as possible.
+                I am available to contact mostly on weekdays. You can reach me via my email at 
+                <strong>amtorres2@student.apc.edu.ph</strong> or on Facebook at 
+                <strong><a href="https://www.facebook.com/andreiluis.torres" target="_blank">https://www.facebook.com/andreiluis.torres</a></strong>.
+                Whether you have a question, want to discuss an idea, or just want to get in touch, feel free to send me a message anytime. 
+                I’ll do my best to respond as quickly as possible.
             </p>
-        `,
+`,
+
 
     };
 
